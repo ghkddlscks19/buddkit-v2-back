9916240fcdf0edcfd4a34289302f1c911df69200
황인찬 <ghkddlscks19@naver.com>
2026-05-15 23:28:31 +0900

    feat(notification): Notification/NotificationType 엔티티 추가

---
+++ src/main/java/com/buddkitv2/domain/notification/NotificationTypeEnum.java
@@ -0,0 +1,9 @@
+package com.buddkitv2.domain.notification;
+
+public enum NotificationTypeEnum {
+    CHAT,       // 채팅 알림
+    LIKE,       // 좋아요 알림
+    SETTLEMENT, // 정산 알림
+    COMMENT,    // 댓글 알림
+    SCHEDULE    // 정모 생성 알림
+}
