b045c516f8ffa31f98bbfa982f634e5453430f42
황인찬 <ghkddlscks19@naver.com>
2026-05-17 16:10:29 +0900

    feat(club): Club 엔티티 메서드 및 Repository 정의

---
+++ src/main/java/com/buddkitv2/domain/club/repository/ClubRepository.java
@@ -0,0 +1,7 @@
+package com.buddkitv2.domain.club.repository;
+
+import com.buddkitv2.domain.club.entity.Club;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClubRepository extends JpaRepository<Club, Long> {
+}
